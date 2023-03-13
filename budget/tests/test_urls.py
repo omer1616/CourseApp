c3994d5bc5aefb14bca4626ce0c8731019b438b7
Omer <omerinci415@gmail.com>
2023-03-13 17:10:51 +0300

    fsddf

---
+++ budget/tests/test_urls.py
@@ -0,0 +1,18 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from books.views import books, book_detail, home
+from books.models import *
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_list_url_resolved(self):
+        url = reverse('books')
+        # print(resolve(url))
+
+        self.assertEqual(resolve(url).func, books)
+
+    def test_detail_url_resolves(self):
+
+        url = reverse('book_detail', kwargs={'slug': 'slug'})
+        self.assertEqual(resolve(url).func, book_detail)
